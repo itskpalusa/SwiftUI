f8c3589e4b1007ad002e8dd8632756a739558487
Karthik Palusa <karthik.palusa@gmail.com>
2024-01-28 11:37:05 -0800

    SwiftUI Card Component

---
+++ Hike-App/Students/Hike/Hike/Component/CardView.swift
@@ -0,0 +1,34 @@
+//
+//  CardView.swift
+//  Hike
+//
+//  Created by Karthik Palusa on 1/28/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        // MARK: - CARD
+        
+        
+        ZStack {
+            CustomBackgroundView()
+            
+            ZStack {
+                Circle()
+                    .fill(LinearGradient(colors:[Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    ).frame(width: 256, height:256)
+                
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height:570)
+    }
+}
+
+#Preview {
+    CardView()
+}
