c5415cd99455ee61154817c659a830955743ec56
Karthik Palusa <karthik.palusa@gmail.com>
2024-01-28 11:17:08 -0800

    Project Setup

---
+++ Hike-App/Students/Hike/Hike/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Karthik Palusa on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike-App/Students/Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Karthik Palusa on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
