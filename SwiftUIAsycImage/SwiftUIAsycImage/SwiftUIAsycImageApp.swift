7d2f369ed89ddb3ac860f6936ebd77d412486e6c
Karthik Palusa <karthik.palusa@gmail.com>
2024-01-27 21:48:46 -0800

    Async Images

---
+++ SwiftUIAsycImage/SwiftUIAsycImage/SwiftUIAsycImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsycImageApp.swift
+//  SwiftUIAsycImage
+//
+//  Created by Karthik Palusa on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsycImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
