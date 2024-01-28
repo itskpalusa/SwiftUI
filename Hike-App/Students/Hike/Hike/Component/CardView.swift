356920bcdd7b2137ce8e393cfb6379a1368b493b
Karthik Palusa <karthik.palusa@gmail.com>
2024-01-28 11:53:40 -0800

    Card Header View

--- Hike-App/Students/Hike/Hike/Component/CardView.swift
+++ Hike-App/Students/Hike/Hike/Component/CardView.swift
@@ -15,14 +15,47 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(LinearGradient(colors:[Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    ).frame(width: 256, height:256)
+            VStack {
+                // MARK: - HEADER
                 
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(LinearGradient(colors: [Color.customGrayLight, Color.customGrayMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        
+                        Spacer()
+                        
+                        Button {
+                            // ACTION: Show a Sheet
+                            print("The button was pressed.")
+                        } label: {
+                          CustomButtonView()
+                        }
+                    }
+                    
+                    Text("Fun and enjoyable outdoor activities for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(Color.customGrayMedium)
+                } //: HEADER
+                .padding(.horizontal, 30)
+                
+                // MARK: - MAIN CONTENT
+                
+                ZStack {
+                    Circle()
+                        .fill(LinearGradient(colors:[Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                        ).frame(width: 256, height:256)
+                    
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                //MARK: - FOOTER
             }
         } //: CARD
         .frame(width: 320, height:570)
