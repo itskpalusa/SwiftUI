1b368b96000c4bfa90335e5e5bd46b8fc9a827d3
Karthik Palusa <karthik.palusa@gmail.com>
2024-01-28 16:29:01 -0800

    SwiftUI Sheet

--- Hike-App/Students/Hike/Hike/Component/CardView.swift
+++ Hike-App/Students/Hike/Hike/Component/CardView.swift
@@ -12,6 +12,7 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     // MARK: = FUNCTIONS
     
@@ -55,9 +56,15 @@ struct CardView: View {
                         Button {
                             // ACTION: Show a Sheet
                             print("The button was pressed.")
+                            isShowingSheet.toggle()
                         } label: {
                             CustomButtonView()
                         }
+                        .sheet(isPresented: $isShowingSheet, content: {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium,.large])
+                        })
                     }
                     
                     Text("Fun and enjoyable outdoor activities for friends and families.")
