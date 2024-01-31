cef9a85a5389205faec68f1152569d2c998e9ee5
Karthik Palusa <karthik.palusa@gmail.com>
2024-01-30 19:05:45 -0800

    Init Restart App

---
+++ Restart-App/Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Karthik Palusa on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
