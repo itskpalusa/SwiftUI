4c333b7d28285e3522138262a3031c14cd6e71e7
Karthik Palusa <karthik.palusa@gmail.com>
2024-02-06 20:56:33 -0700

    Project Creation in XCode

---
+++ Fruits/Students/Fruits/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Fruits
+//
+//  Created by Karthik Palusa on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fruits/Students/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Karthik Palusa on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
