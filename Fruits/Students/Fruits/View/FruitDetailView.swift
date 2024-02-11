e5e552f8be55cff904789b5e3855d04ff9f16e80
Karthik Palusa <karthik.palusa@gmail.com>
2024-02-11 15:06:34 -0800

    Create Detailed Fruit View

---
+++ Fruits/Students/Fruits/View/FruitDetailView.swift
@@ -0,0 +1,65 @@
+//
+//  FruitDetailView.swift
+//  Fruits
+//
+//  Created by Karthik Palusa on 2/11/24.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    //MARK: - PROPERTIES
+    
+    var fruit: Fruit
+    
+    //MARK: - BODY
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+                    // HEADER
+                    FruitHeaderView(fruit: fruit)
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        // TITLE
+                        Text(fruit.title)
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // HEADLINE
+                        Text(fruit.headline)
+                            .font(.headline)
+                            .multilineTextAlignment(.leading)
+                        
+                        // NUTRIENTS
+                        
+                        // SUBHEADLINE
+                        Text("Learn more about \(fruit.title)".uppercased())
+                            .fontWeight(.bold)
+                            .foregroundColor(fruit.gradientColors[1])
+                        
+                        // DESCRIPTION
+                        Text(fruit.description)
+                            .multilineTextAlignment(.leading)
+                        
+                        // LINK
+                        SourceLinkView()
+                            .padding(.top, 10)
+                            .padding(.bottom, 40)
+                    } //: VSTACK
+                    .padding(.horizontal, 20)
+                    .frame(maxWidth: 640, alignment: .center)
+                } //: VSTACK
+                .navigationBarTitle(fruit.title, displayMode: .inline)
+                .navigationBarHidden(true)
+            } //: SCROLL
+            .edgesIgnoringSafeArea(.top)
+        } //: NAVIGATION
+    }
+}
+
+//MARK: - PREVIEW
+#Preview {
+    FruitDetailView(fruit: fruitsData[0])
+}
---
+++ Fruits/Students/Fruits/View/SourceLinkView.swift
@@ -0,0 +1,28 @@
+//
+//  SourceLinkView.swift
+//  Fruits
+//
+//  Created by Karthik Palusa on 2/11/24.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox() {
+            HStack {
+                Text("Content Source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            }
+            .font(.footnote)
+        }
+    }
+}
+
+#Preview {
+    SourceLinkView()
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
