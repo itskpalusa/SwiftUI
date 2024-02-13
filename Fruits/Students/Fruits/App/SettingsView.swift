5b58ea5fc613261030db494a5c755a4c8337454e
Karthik Palusa <karthik.palusa@gmail.com>
2024-02-13 12:14:30 -0800

    Finish Settings Page

--- Fruits/Students/Fruits/App/SettingsView.swift
+++ Fruits/Students/Fruits/App/SettingsView.swift
@@ -11,6 +11,7 @@ struct SettingsView: View {
     //MARK: - PROPERTIES
     
     @Environment(\.presentationMode) var presentationMode
+    @AppStorage("isOnboarding") var isOnboarding: Bool = false
     
     //MARK: - BODY
     var body: some View {
@@ -39,17 +40,44 @@ struct SettingsView: View {
                     }
                     
                     // MARK: - SECTION 2
+                    GroupBox(
+                        label: SettingsLabelView(labelText: "Customization", labelImage: "paintbrush")
+                    ) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("Restart application onboarding by using toggle. This toggle will restart the application onboarding process and will display the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnboarding) {
+                            if isOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(Color(UIColor.tertiarySystemBackground)
+                            .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous)))
+                    }
                     
                     // MARK: - SECTION 3
-                    GroupBox( 
+                    GroupBox(
                         label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")
-                        ) {
-                            
-                            SettingsRowView(name: "Developer", content: "Karthik Palusa")
-                            SettingsRowView(name: "Designer", content: "Karthik Palusa")
-                            SettingsRowView(name: "Compatibility", content:"iOS 17")
-                            SettingsRowView(name: "Website", linkLabel: "Personal Website", linkDestination: "karthikpalusa.com")
-                        }//: BOX
+                    ) {
+                        
+                        SettingsRowView(name: "Developer", content: "Karthik Palusa")
+                        SettingsRowView(name: "Designer", content: "Karthik Palusa")
+                        SettingsRowView(name: "Compatibility", content:"iOS 17")
+                        SettingsRowView(name: "Website", linkLabel: "Personal Website", linkDestination: "karthikpalusa.com")
+                    }//: BOX
                     
                 }//: VSTACK
                 .navigationBarTitle(Text("Settings"), displayMode: .large)
@@ -57,9 +85,7 @@ struct SettingsView: View {
                     presentationMode.wrappedValue.dismiss()
                 }) {
                     Image(systemName: "xmark")
-                }
-                                    
-                )
+                })
                 .padding()
             }//: SCROLLVIEW
         }//: NAVIGATION
